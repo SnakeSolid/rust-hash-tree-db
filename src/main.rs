08f0f78f49f1b9458471b59d0b8509e48d3a8487
Shabanov Anton <snakesolid@ngs.ru>
2022-01-16 13:49:11 +0300

    command line options

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,34 @@
 extern crate log;
 
 mod command;
+mod options;
 
 use crate::command::Command;
+use crate::options::Options;
 use hash_tree_db::Config;
 use hash_tree_db::Database;
 use hash_tree_db::PrintVisiter;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use structopt::StructOpt;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-type Data = String;
-
 fn main() {
     env_logger::init();
 
-    info!("starting up");
+    info!("Starting up");
 
+    let options = Options::from_args();
     let config = Config::default()
-        .set_max_page_size(10)
-        .set_max_pages(Some(2))
-        .set_storage_path("storage");
-    let mut database: Database<Data, Data, Data> = Database::new(config);
+        .set_max_page_size(options.page_size())
+        .set_max_pages(options.memory_pages())
+        .set_storage_path(options.storage_path());
+    let mut database: Database<String, String, String> = Database::new(config);
     let mut editor = Editor::<()>::new();
+
+    info!("Entered to REPL mode");
 
     loop {
         let input = match editor.readline(">> ") {
@@ -38,82 +42,90 @@ fn main() {
             }
         };
 
-        match Command::parse(&input) {
-            Ok(Command::Get { hash_key, tree_key }) => {
-                let hash_key = hash_key.into();
-                let tree_key = tree_key.into();
+        for input in input.split('\n') {
+            let input = input.trim();
 
-                match database.get(&hash_key, &tree_key) {
-                    Ok(Some(data)) => println!("OK {} {} {}", hash_key, tree_key, data),
-                    Ok(None) => println!("OK"),
-                    Err(error) => println!("ERR {}", error),
-                }
-            }
-            Ok(Command::Put {
-                hash_key,
-                tree_key,
-                data,
-            }) => {
-                let hash_key = hash_key.into();
-                let tree_key = tree_key.into();
-                let data = data.into();
-
-                match database.put(hash_key, tree_key, data) {
-                    Ok(_replaced) => println!("OK"),
-                    Err(error) => println!("ERR {}", error),
-                }
-            }
-            Ok(Command::Contains { hash_key, tree_key }) => {
-                let hash_key = hash_key.into();
-                let tree_key = tree_key.into();
-
-                match database.contains(&hash_key, &tree_key) {
-                    Ok(true) => println!("OK TRUE"),
-                    Ok(false) => println!("OK FALSE"),
-                    Err(error) => println!("ERR {}", error),
-                }
-            }
-            Ok(Command::Delete { hash_key, tree_key }) => {
-                let hash_key = hash_key.into();
-                let tree_key = tree_key.into();
-
-                match database.delete(&hash_key, &tree_key) {
-                    Ok(true) => println!("OK TRUE"),
-                    Ok(false) => println!("OK FALSE"),
-                    Err(error) => println!("ERR {}", error),
-                }
-            }
-            Ok(Command::Range { .. }) => {
-                println!("ERR Command not implemented");
+            if input.is_empty() {
                 continue;
             }
-            Ok(Command::Count {}) => match database.count() {
-                Ok(count) => println!("OK {}", count),
-                Err(error) => println!("ERR {}", error),
-            },
-            Ok(Command::Show {}) => {
-                database.visit(&mut PrintVisiter::default());
 
-                println!("OK");
-            }
-            Ok(Command::Save {}) => {
-                println!("ERR Command not implemented");
-                continue;
-            }
-            Ok(Command::Load {}) => {
-                println!("ERR Command not implemented");
-                continue;
-            }
-            Ok(Command::Exit {}) => {
-                break;
-            }
-            Err(error) => {
-                println!("{}", error);
+            match Command::parse(&input) {
+                Ok(Command::Get { hash_key, tree_key }) => {
+                    let hash_key = hash_key.into();
+                    let tree_key = tree_key.into();
 
-                continue;
+                    match database.get(&hash_key, &tree_key) {
+                        Ok(Some(data)) => println!("OK {} {} {}", hash_key, tree_key, data),
+                        Ok(None) => println!("OK"),
+                        Err(error) => println!("ERR {}", error),
+                    }
+                }
+                Ok(Command::Put {
+                    hash_key,
+                    tree_key,
+                    data,
+                }) => {
+                    let hash_key = hash_key.into();
+                    let tree_key = tree_key.into();
+                    let data = data.into();
+
+                    match database.put(hash_key, tree_key, data) {
+                        Ok(_replaced) => println!("OK"),
+                        Err(error) => println!("ERR {}", error),
+                    }
+                }
+                Ok(Command::Contains { hash_key, tree_key }) => {
+                    let hash_key = hash_key.into();
+                    let tree_key = tree_key.into();
+
+                    match database.contains(&hash_key, &tree_key) {
+                        Ok(true) => println!("OK TRUE"),
+                        Ok(false) => println!("OK FALSE"),
+                        Err(error) => println!("ERR {}", error),
+                    }
+                }
+                Ok(Command::Delete { hash_key, tree_key }) => {
+                    let hash_key = hash_key.into();
+                    let tree_key = tree_key.into();
+
+                    match database.delete(&hash_key, &tree_key) {
+                        Ok(true) => println!("OK TRUE"),
+                        Ok(false) => println!("OK FALSE"),
+                        Err(error) => println!("ERR {}", error),
+                    }
+                }
+                Ok(Command::Range { .. }) => {
+                    println!("ERR Command not implemented");
+                    continue;
+                }
+                Ok(Command::Count {}) => match database.count() {
+                    Ok(count) => println!("OK {}", count),
+                    Err(error) => println!("ERR {}", error),
+                },
+                Ok(Command::Show {}) => {
+                    database.visit(&mut PrintVisiter::default());
+
+                    println!("OK");
+                }
+                Ok(Command::Save {}) => {
+                    println!("ERR Command not implemented");
+                    continue;
+                }
+                Ok(Command::Load {}) => {
+                    println!("ERR Command not implemented");
+                    continue;
+                }
+                Ok(Command::Exit {}) => {
+                    break;
+                }
+                Err(error) => {
+                    println!("{}", error);
+
+                    continue;
+                }
             }
+
+            editor.add_history_entry(input);
         }
-
-        editor.add_history_entry(&input);
     }
 }
---
+++ src/options.rs
@@ -0,0 +1,29 @@
+use std::path::{Path, PathBuf};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "hash-tree-db")]
+pub struct Options {
+    #[structopt(short, long, default_value = "8192")]
+    page_size: usize,
+
+    #[structopt(short, long)]
+    memory_pages: Option<usize>,
+
+    #[structopt(short, long, default_value = ".", parse(from_os_str))]
+    storage_path: PathBuf,
+}
+
+impl Options {
+    pub fn page_size(&self) -> usize {
+        self.page_size
+    }
+
+    pub fn memory_pages(&self) -> Option<usize> {
+        self.memory_pages.clone()
+    }
+
+    pub fn storage_path(&self) -> &Path {
+        self.storage_path.as_path()
+    }
+}
